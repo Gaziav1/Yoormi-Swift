fce3b35b11b131be1bdc735ac2f46c23cd89a591
Gaziav <gaziavishakov2211@icloud.com>
2020-12-22 19:27:09 +0300

    Image picking logic

--- Pet Me/Classes/Modules/ImageAndName/View/ImageAndNameViewController.swift
+++ Pet Me/Classes/Modules/ImageAndName/View/ImageAndNameViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ImageAndNameViewController: UIViewController, ImageAndNameViewInput {
-
+class ImageAndNameViewController: UIViewController {
+    
     var output: ImageAndNameViewOutput!
     
+    private let imagePickerController: UIImagePickerController = {
+        let ip = UIImagePickerController()
+        ip.allowsEditing = true
+        return ip
+    }()
+    
     private let disposeBag = DisposeBag()
-
+    
     private let addPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.snp.makeConstraints {
             $0.size.equalTo(LayoutConstants.AddPhotoButton.size)
         }
-   
+        
         button.backgroundColor = .systemGray6
         button.setImage(R.image.icons.camera()?.withRenderingMode(.alwaysOriginal), for: .normal)
         return button
@@ -36,10 +42,10 @@ class ImageAndNameViewController: UIViewController, ImageAndNameViewInput {
     }()
     
     private let nameTextField: RegistrationTextField = {
-       let tf = RegistrationTextField(text: "Ваше имя")
+        let tf = RegistrationTextField(text: "Ваше имя")
         tf.textField.textContentType = .name
         tf.textField.keyboardType = .alphabet
-       return tf
+        return tf
     }()
     
     private let continueButton: UIButton = {
@@ -48,19 +54,20 @@ class ImageAndNameViewController: UIViewController, ImageAndNameViewInput {
         button.setTitle("Готово", for: .normal)
         button.setTitleColor(.systemGray6, for: .normal)
         button.layer.cornerRadius = 10
+      
         return button
     }()
     
-    private let popUpPhotoView: PopUpView = PopUpView()
+    private let popUpPhotoView = PopUpView()
     
     
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         output.viewIsReady()
+        
     }
-
-
+    
     // MARK: Layout setup
     private func setupButton() {
         view.addSubview(addPhotoButton)
@@ -68,7 +75,7 @@ class ImageAndNameViewController: UIViewController, ImageAndNameViewInput {
             $0.centerX.equalToSuperview()
             $0.centerY.equalToSuperview().offset(-30)
         })
-    
+        
         addPhotoButton.layer.cornerRadius = LayoutConstants.AddPhotoButton.size / 2
         
         addPhotoButton.rx.controlEvent(.touchUpInside).bind {
@@ -109,17 +116,58 @@ class ImageAndNameViewController: UIViewController, ImageAndNameViewInput {
         popUpPhotoView.snp.makeConstraints({ $0.edges.equalToSuperview() })
     }
     
-    // MARK: ImageAndNameViewInput
-    func setupInitialState() {
-        view.backgroundColor = .white
-        setupButton()
-       // setupAddPhotoLabel()
-        setupNameTextField()
-        setupContinueButton()
-        setupPopUpView()
+    
+    private func setupSubcriptions() {
+        popUpPhotoView.popViewItemsTappingObservable.subscribe(onNext: { [weak self] tappedItemType in
+            self?.showImagePicker(fromType: tappedItemType)
+        }).disposed(by: disposeBag)
+    }
+    
+    private func showImagePicker(fromType type: PopViewItemType) {
+        switch type {
+        case .takePhoto:
+            imagePickerController.sourceType = .camera
+        case .choosePhoto:
+            imagePickerController.sourceType = .photoLibrary
+        case .noPhoto:
+            addPhotoButton.setImage(R.image.icons.camera(), for: .normal)
+            return
+        }
+        
+        present(imagePickerController, animated: true, completion: nil)
     }
 }
 
+// MARK: - ImageAndNameViewInput
+extension ImageAndNameViewController: ImageAndNameViewInput {
+    
+    func setupInitialState() {
+        imagePickerController.delegate = self
+        view.backgroundColor = .white
+        setupButton()
+        // setupAddPhotoLabel()
+        setupNameTextField()
+        setupContinueButton()
+        setupPopUpView()
+        setupSubcriptions()
+    }
+}
+
+
+// MARK: - UIImagePickerControllerDelegate & UINavigationControllerDelegate
+extension ImageAndNameViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let photo = info[.originalImage] as? UIImage else {
+            print("NO IMAGE")
+            return
+        }
+        
+        addPhotoButton.setImage(photo.withRenderingMode(.alwaysOriginal), for: .normal)
+        addPhotoButton.imageView?.layer.cornerRadius = LayoutConstants.AddPhotoButton.size / 2
+    }
+}
 
 
 private enum LayoutConstants {
